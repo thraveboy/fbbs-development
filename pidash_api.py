e3c1ab13b7f5e13b51031592236b0179a134fd73
Reid Gershbein <reid.gershbein@gmail.com>
2017-07-21 22:29:13 +0000

    Updated install script to restart apache server once changes updated

---
+++ pidash_api.py
@@ -0,0 +1,30 @@
+import requests
+
+class PiDashAPI:
+    """Class object for interacting with PiDash API"""
+    def __init__(self, in_host, in_username, in_password, in_token=""):
+        self.host = in_host
+        self.username = in_username
+        self.password = in_password
+        self.token = in_token
+    def set_command(self, table_name, data):
+        self.table_name = table_name
+        self.data = data
+    def send(self):
+        token_request = requests.post(self.host, data={'username': self.username, 'password': self.password})
+        print(token_request.status_code, token_request.reason)
+        print(token_request.text)
+api_test_obj = PiDashAPI("http://localhost/pidash-api.php", "test_user", "test_password")
+
+print api_test_obj.username
+print api_test_obj.password
+print api_test_obj.host
+print api_test_obj.token
+
+api_test_obj.set_command("test_table", "test data")
+print api_test_obj.table_name
+print api_test_obj.data
+api_test_obj.send()
+
+
+

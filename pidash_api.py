edcf5c7521197ea835a7f69da559ebb55c196ea1
Reid Gershbein <reid.gershbein@gmail.com>
2017-07-22 15:49:47 +0000

    Added python api wrapper to pidash

--- pidash_api.py
+++ pidash_api.py
@@ -1,36 +1,34 @@
 import requests
 import json
+import sys
 
 class PiDashAPI:
     """Class object for interacting with PiDash API"""
-    def __init__(self, in_host, in_username, in_password, in_token=""):
+    def __init__(self, in_host="http://localhost/pidash-api.php", in_username="SysOp", in_password="sysop", in_token=""):
         self.host = in_host
         self.username = in_username
         self.password = in_password
         self.token = in_token
-    def set_command(self, table_name, data):
-        self.table_name = table_name
-        self.data = data
+        self.command = ""
+    def set_command(self, command):
+        self.command = command
     def send(self):
         token_request = requests.post(self.host, data={'username': self.username, 'password': self.password})
-        print(token_request.status_code, token_request.reason)
-        print(token_request.text)
         json_decoder = json.JSONDecoder()
         decoded_json = json_decoder.decode(token_request.text)
+        if "user_created" in decoded_json:
+            token_request = requests.post(self.host, data={'username': self.username, 'password': self.password})
+            decoded_json = json_decoder.decode(token_request.text)
         if "token" in decoded_json:
-          print(decoded_json["token"])
+            self.token = decoded_json["token"]
+            send_command = requests.post(self.host, data={'username': self.username, 'token': self.token, 'command' : self.command})
+            print(send_command.text)
 
-api_test_obj = PiDashAPI("http://localhost/pidash-api.php", "test1_user", "test1_password")
-
-print api_test_obj.username
-print api_test_obj.password
-print api_test_obj.host
-print api_test_obj.token
-
-api_test_obj.set_command("test_table", "test data")
-print api_test_obj.table_name
-print api_test_obj.data
-api_test_obj.send()
+if (len(sys.argv) > 1):
+    api_test_obj = PiDashAPI()
+    print('command:' + ' '.join(sys.argv[1:]))
+    api_test_obj.set_command(' '.join(sys.argv[1:]))
+    api_test_obj.send()
 
 
 

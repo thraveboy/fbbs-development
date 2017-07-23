a38dd2df1f8a182d00992155dc9b5291f26e8c7a
Reid Gershbein <reid.gershbein@gmail.com>
2017-07-23 02:50:23 +0000

    Added daemon example for percentage of file space available

---
+++ SCRIPTS/hostname_percent_file_avail_daemon.py
@@ -0,0 +1,12 @@
+import subprocess
+import time
+
+hostname = subprocess.check_output('hostname').strip('\n')
+total_space = subprocess.check_output("stat -f -c\"%b\" /", shell=True).strip('\n')
+
+while 1:
+    avail_space = subprocess.check_output("stat -f -c\"%a\" /", shell=True).strip('\n')
+    percentage_free = int((float(avail_space) / float(total_space)) * 100.0)
+    api_command_str = "python fbbs_api.py " + hostname + "_avail_disk '" + str(percentage_free) + "%'"
+    fbbs_api_obj = subprocess.check_output(api_command_str, shell=True)
+    time.sleep(10)

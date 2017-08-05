6834070fea59bb9034142096c78ccd6712dfcef2
Reid Gershbein <reid.gershbein@gmail.com>
2017-08-05 15:23:25 +0000

    Added BTC ticker tracker daemon to populate graphs.

--- SCRIPTS/crypto_currency.py
+++ SCRIPTS/crypto_currency.py
@@ -1,5 +1,6 @@
 import subprocess
 import string
+
 ticker_values = subprocess.check_output('curl https://www.worldcoinindex.com/ | sed -f coin-scrap.sed | sed -f sed-clean.sed | grep Ticker', shell=True)
 ticker_value_array = string.split(ticker_values,'\n')
 ticker_values_in_array = []
@@ -7,8 +8,7 @@ for ticker_value in ticker_value_array:
     print(ticker_value)
     ticker_values_in_array.append(string.split(ticker_value,' '))
 for ticker_value_in_array in ticker_values_in_array:
-    print(ticker_value_in_array[0])
-    print(ticker_value_in_array[1])
-
-#api_command_str = "python fbbs_api.py " + "monitor_" + hostname + "_memused '" + str(mem_used_percentage) + "%'"
-# fbbs_api_obj = subprocess.check_output(api_command_str, shell=True)
+    if ticker_value_in_array[0] == "Ticker:BTC":
+        ticker_value = str(ticker_value_in_array[1])
+        api_command_str = "python fbbs_api.py " + str(ticker_value_in_array[0]) + " " + str(ticker_value)
+        fbbs_api_obj = subprocess.check_output(api_command_str, shell=True)
---
+++ SCRIPTS/crypto_currency_daemon.py
@@ -0,0 +1,17 @@
+import subprocess
+import string
+import time
+
+while 1:
+    ticker_values = subprocess.check_output('curl https://www.worldcoinindex.com/ | sed -f coin-scrap.sed | sed -f sed-clean.sed | grep Ticker', shell=True)
+    ticker_value_array = string.split(ticker_values,'\n')
+    ticker_values_in_array = []
+    for ticker_value in ticker_value_array:
+        print(ticker_value)
+        ticker_values_in_array.append(string.split(ticker_value,' '))
+    for ticker_value_in_array in ticker_values_in_array:
+        if ticker_value_in_array[0] == "Ticker:BTC":
+            ticker_value = str(ticker_value_in_array[1])
+            api_command_str = "python fbbs_api.py " + str(ticker_value_in_array[0]) + " " + str(ticker_value)
+            fbbs_api_obj = subprocess.check_output(api_command_str, shell=True)
+    time.sleep(10)

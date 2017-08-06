a9ac9efc9e7fbb9664de53cc218715f37ac3d355
Reid Gershbein <reid.gershbein@gmail.com>
2017-08-06 15:03:36 +0000

    Added input argument to crypto currency script to allow to customize rate of getting values and makes appropriate table

--- SCRIPTS/crypto_currency_daemon.py
+++ SCRIPTS/crypto_currency_daemon.py
@@ -1,6 +1,11 @@
 import subprocess
 import string
 import time
+import sys
+
+update_rate_mins = 5
+if (len(sys.argv) > 1):
+    update_rate_mins = float(sys.argv[1])
 
 while 1:
     ticker_values = subprocess.check_output('curl https://www.worldcoinindex.com/ | sed -f coin-scrap.sed | sed -f sed-clean.sed | grep Ticker', shell=True)
@@ -12,6 +17,7 @@ while 1:
     for ticker_value_in_array in ticker_values_in_array:
         if (ticker_value_in_array[0] == "Ticker:BTC") or (ticker_value_in_array[0] == "Ticker:BCC") or (ticker_value_in_array[0] == "Ticker:ETH"):
             ticker_value = str(ticker_value_in_array[1])
-            api_command_str = "python fbbs_api.py " + str(ticker_value_in_array[0]) + " " + str(ticker_value)
+            api_command_str = "python fbbs_api.py '" + str(ticker_value_in_array[0]) + ":" + str(update_rate_mins) + "min' " + str(ticker_value)
+            print(api_command_str)
             fbbs_api_obj = subprocess.check_output(api_command_str, shell=True)
-    time.sleep(30)
+    time.sleep(60 * update_rate_mins)

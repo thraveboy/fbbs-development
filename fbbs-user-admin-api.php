6331a086184f8c8cb93dd515aca01d2b334d0dc5
Fury <reid.gershbein@gmail.com>
2017-08-15 17:11:48 +0000

    Added retrieval of user attributes to user-admin api

--- fbbs-user-admin-api.php
+++ fbbs-user-admin-api.php
@@ -1,4 +1,12 @@
 <?php
+  $user_attr_fields = [];
+  $user_attr_fields['CAN_READ'] = "can_read";
+  $user_attr_fields['CAN_WRITE'] = "can_write";
+  $user_attr_fields['CREATE_TABLE'] = "create_table";
+  $user_attr_fields['IS_ADMIN'] = "is_admin";
+
+  echo $user_attr_fields['CAN_READ'] . PHP_EOL;
+
   class FDBUSER extends mysqli
   {
     private $fbbs_servername = "localhost";
@@ -16,19 +24,41 @@
     }
   }
 
-  function get_user_attributes($user_id=0) {
+  function get_user_attributes($username="") {
    $user_attributes = [];
-   if ($user_id > 0) {
+   $user_id = 0;
+   if ($username != "") {
       $fdbuser = new FDBUSER();
       if (!$fdbuser) {
         echo $fdbuser->error;
       }
-      $username_query = "SELECT username FROM users where id = " . $user_id;
-      $username_result = $fdbuser->query($username_query);
-      if ($username_result->num_rows > 0) {
-        $username_array = $username_result->fetch_assoc();
-        $username = $username_array['username'];
-        $user_attributes['username'] = $username;
+      $clean_username = $fdbuser->real_escape_string($username);
+      $user_attributes['username'] = $clean_username;
+      $userid_query = "SELECT id FROM users where username = '" .
+        $clean_username . "'";
+      $userid_result = $fdbuser->query($userid_query);
+      if ($userid_result->num_rows > 0) {
+        $userid_array = $userid_result->fetch_assoc();
+        $user_id = $userid_array['id'];
+        $user_attributes['user_id'] = $user_id;
+        if ($user_id > 0) {
+          $userattr_query = "SELECT * from user_attr WHERE userid = " .
+            $user_id;
+          $userattr_result = $fdbuser->query($userattr_query);
+          if ($userattr_result->num_rows > 0) {
+            while ($userattr_row = $userattr_result->fetch_assoc()) {
+              $attr_id = $userattr_id['id'];
+              $attr_name = $userattr_row['attribute'];
+              $attr_value = $userattr_row['value'];
+              $attr_usermod = $userattr_row['user_mod'];
+              $user_attributes[$attr_id] = array(
+                  "attribute" => $attr_name,
+                  "value" =>  $attr_value,
+                  "user_mod" => $attr_usermod
+                );
+            }
+          }
+        }
       }
     }
     return $user_attributes;
--- test-user-attributes.php
+++ test-user-attributes.php
@@ -2,7 +2,7 @@
 
 require_once "fbbs-user-admin-api.php";
 
-$test_user_attributes = get_user_attributes(1);
+$test_user_attributes = get_user_attributes("happy");
 
 print_r($test_user_attributes);
 
